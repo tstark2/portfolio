61dc81233b682802449aa1889ffc3d50804a4ae5
Todd Stark II <ackilliez@gmail.com>
2019-10-19 18:23:55 -0500

    Working on content

---
+++ code/js/index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const examples = document.querySelectorAll('#work .example');
+    for(const example of examples) {
+        example.addEventListener('mouseover', e => {
+            const flyover = e.currentTarget.querySelector('.flyover');
+            flyover.classList.add('open');
+        });
+
+        example.addEventListener('mouseout', e => {
+            const flyover = e.currentTarget.querySelector('.flyover');
+            flyover.classList.remove('open');
+        });
+
+        example.addEventListener('click', e => {
+            const name = e.currentTarget.dataset.name
+            window.location.assign(`workDetails.html?name=${name}`);
+        });
+    }
+
+    document.getElementById('menuButton').addEventListener('click', e => {
+        document.getElementsByTagName('nav')[0].classList.add('open');
+
+    });
+
+    document.getElementsByTagName('nav')[0].addEventListener('click', e => {
+        e.currentTarget.classList.remove('open');
+    });
+});
