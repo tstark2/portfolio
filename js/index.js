fcbe0ef3086c2363f6bfa551d47cb6b7b348170c
Todd Stark II <ackilliez@gmail.com>
2019-10-19 14:58:29 -0500

    Started on work section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+
+    const examples = document.querySelectorAll('#work .example');
+    for(const example of examples) {
+        example.addEventListener('mouseover', e => {
+            const flyover = e.currentTarget.querySelector('.flyover');
+            flyover.classList.add('open');
+        });
+    }
+});
