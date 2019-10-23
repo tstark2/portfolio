1560a5d886337986ff09a80252eae373f5ee234c
Todd Stark II <ackilliez@gmail.com>
2019-10-23 17:24:08 -0500

    Decided on a design after all

--- code/js/index.js
+++ code/js/index.js
@@ -11,19 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
             const flyover = e.currentTarget.querySelector('.flyover');
             flyover.classList.remove('open');
         });
-
-        // example.addEventListener('click', e => {
-        //     const name = e.currentTarget.dataset.name
-        //     window.location.assign(`workDetails.html?name=${name}`);
-        // });
     }
-
-    document.getElementById('menuButton').addEventListener('click', e => {
-        document.getElementsByTagName('nav')[0].classList.add('open');
-
-    });
-
-    document.getElementsByTagName('nav')[0].addEventListener('click', e => {
-        e.currentTarget.classList.remove('open');
-    });
 });
