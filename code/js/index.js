593c56f2920b9406756e10de0843db9574ff20d4
Todd Stark II <ackilliez@gmail.com>
2019-10-26 17:09:09 -0500

    Updated Resume Updated Gitlab Links

--- code/js/index.js
+++ code/js/index.js
@@ -2,24 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const examples = document.querySelectorAll('#work .example');
     for(const example of examples) {
-        example.addEventListener('mouseover', e => {
-            const flyover = e.currentTarget.querySelector('.flyover');
-            flyover.classList.add('open');
-        });
-
-        example.addEventListener('mouseout', e => {
-            const flyover = e.currentTarget.querySelector('.flyover');
-            flyover.classList.remove('open');
-        });
-
-        example.addEventListener('touchend', e => {
-            e.preventDefault();
+        example.addEventListener('touchstart', e => {
             const flyover = e.currentTarget.querySelector('.flyover');
 
-            if(flyover.classList.contains('open')) {
-                flyover.classList.remove('open');
-            } else {
-                flyover.classList.add('open');
+            if(e.target.tagName != 'A') {
+                if(flyover.classList.contains('open')) {
+                    flyover.classList.remove('open');
+                } else {
+                    flyover.classList.add('open');
+                }
             }
         });
     }
