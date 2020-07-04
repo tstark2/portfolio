7c1dfdf2aeaa6112e574a3a107642c865d6a019a
Todd Stark II <ackilliez@gmail.com>
2020-07-04 11:16:20 -0500

    Update resume - add better touch support for examples

--- code/js/index.js
+++ code/js/index.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const openButtons = document.getElementsByClassName('opener');
+    
+    for(const button of openButtons) {
+        button.addEventListener('touchstart', e => {
+            const opened = document.querySelectorAll('.flyover.open');
+            for(const open of opened) {
+                open.classList.remove('open');
+            }
 
-    // if touch is enabled, respond to touch events
-    window.addEventListener('touchstart', () => {
-        document.getElementById('work').classList.remove('hover');
-    });
+            const flyover = e.currentTarget.parentElement.querySelector('.flyover');
+            flyover.classList.add('open');
+        });
+    }
 
-    const examples = document.querySelectorAll('#work:not(hover) .example');
-    for(const example of examples) {
-        example.addEventListener('touchend', e => {
-            const flyover = e.currentTarget.querySelector('.flyover');
-
-            if(e.target.tagName != 'A') {
-                if(flyover.classList.contains('open')) {
-                    flyover.classList.remove('open');
-                } else {
-                    flyover.classList.add('open');
-                }
+    const flyovers = document.getElementsByClassName('flyover');
+    for(const flyover of flyovers) {
+        flyover.addEventListener('touchstart', e => {
+            if(e.target.tagName !== 'A') {
+                flyover.classList.remove('open');
             }
         });
     }
